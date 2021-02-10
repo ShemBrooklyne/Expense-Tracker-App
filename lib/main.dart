191cc9b811ccff05279d06343552a79361cc9186
ShemBrooklyne <shemnyatti@gmail.com>
2021-02-10 18:20:16 +0300

    transactions main method

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:expense_tracker/transaction.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -13,6 +14,20 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
+  final List<Transaction> transactions = [
+    Transaction(
+      id: 't1',
+      title: 'New Sneakers',
+      amount: 69.99,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: 'Gaming Mouse',
+      amount: 23.50,
+      date: DateTime.now(),
+    ),
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -31,10 +46,12 @@ class MyHomePage extends StatelessWidget {
               elevation: 5,
             ),
           ),
-          Card(
-            color: Colors.red,
-            child: Text('LIST OF TX'),
-          )
+          Column(children: transactions.map((tx) {
+            return Card(
+              child: Text(tx.title),
+            );
+          }).toList(), 
+          ),
         ],
       ),
     );
